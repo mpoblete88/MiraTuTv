1050195fa837131262552a776a30d4a5be6d09cc
Moises Poblete <moises.poblete88@gmail.com>
2017-10-22 17:44:44 -0300

    merge

--- public/js/app.js
+++ public/js/app.js
@@ -41911,7 +41911,7 @@ var Component = normalizeComponent(
   __vue_scopeId__,
   __vue_module_identifier__
 )
-Component.options.__file = "resources/assets/js/components/Example.vue"
+Component.options.__file = "resources\\assets\\js\\components\\Example.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key.substr(0, 2) !== "__"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] Example.vue: functional components are not supported with templates, they should use render functions.")}
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -16,4 +16,4 @@ mix.js('resources/assets/js/app.js', 'public/js')
 
 mix.copy('resources/assets/images','public/images');
 mix.copy('resources/assets/images/favicon.ico','public/');
-mix.copy('resources/assets/js/libraries','public/js');
+// mix.copy('resources/assets/js/libraries','public/js');
