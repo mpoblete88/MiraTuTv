868cb87e2c760dc8f03f7783b811770d1491f6d6
Moises Poblete <moises.poblete88@gmail.com>
2017-10-31 00:24:43 -0300

    creacion de listado de canales para seeder plan basico

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,11 +11,11 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js');
+mix.sass('resources/assets/sass/app.scss', 'public/css');
 
 mix.copy('resources/assets/images','public/images');
 mix.copy('resources/assets/images/favicon.ico','public/');
+
 //mix.copy('resources/assets/js/libraries','public/js');
  mix.copy('node_modules/animate.css/animate.min.css','public/css/animate.min.css');
->>>>>>> 19805ea0000b811772361db96e399e3c3f05a2b8
