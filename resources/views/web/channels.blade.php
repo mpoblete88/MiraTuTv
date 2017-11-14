6aab7f62de7eb790128bc129edb4842d66ae86c3
Edward Salas <edwardsala@gmail.com>
2017-11-13 21:43:07 -0300

    rutas modal

---
+++ resources/views/web/channels.blade.php
@@ -0,0 +1,10 @@
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal">&times;</button>
+    <h4 class="modal-title">Modal Header</h4>
+</div>
+<div class="modal-body">
+    <h1>Plan X Canales</h1>
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
