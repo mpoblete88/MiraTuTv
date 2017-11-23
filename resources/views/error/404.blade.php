cb1e7f2be482e9b97a3aee67b2637cc6895528f6
Edward Salas <edwardsala@gmail.com>
2017-11-22 23:25:06 -0300

    error 404

---
+++ resources/views/error/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.web')
+@section('content')
+<section class="" style="background: #f9f9f9; height: 500px">
+    <div class="col-lg-6 col-lg-offset-3" style=" padding-top: 100px ">
+        <img  style="margin: 40px;" class="pull-left" src="{{url('/images/icon-error.svg')}}" alt="Generic placeholder " width="200" height="200">
+        <H4 style="font-weight: 900; color: #cccccc; margin: 0; margin-top: 50px">O<span>OPS!</span></H4>
+        <h1 style="color:#E91D62;margin: 0;padding: 0">ERROR <b style="font-size: 48px">404</b>   </h1>
+        <H3 style="margin: 0">PAGINA NO <br>ENCONTRADA</H3>
+    </div>
+
+</section>
+@endsection
