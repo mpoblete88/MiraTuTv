f7b2e304dd4a75a9f0bdaa97310577bafeb152f9
Edward Salas <edwardsala@gmail.com>
2017-11-26 20:58:50 -0300

    Add font family

--- resources/views/web/plans.blade.php
+++ resources/views/web/plans.blade.php
@@ -37,7 +37,7 @@
                         <div class="col-lg-6" style="padding: 2px">
                             <span class="label label-default pull-left selectDeco" style="width: 100%; padding: 7px"> <i class="fa fa-plus"></i><i class="icon-deco"></i> 2 Deco </span>
 
-                            <span class="label label-default pull-left selectDeco" style="width: 100%; padding: 7px; margin-top: 3px"> base + base </span>
+                            <span class="label label-default pull-left selectDeco" style="width: 100%; padding: 7px; margin-top: 3px"> Base + Base </span>
 
                         </div>
                         <div class="col-lg-6" style="padding: 2px">
