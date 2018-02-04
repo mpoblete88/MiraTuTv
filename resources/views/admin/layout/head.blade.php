017461c56c43c30a81050b75ac89908e70c40e8a
Jose Nuñez <jnunez@arqz-bim.com>
2018-02-04 03:11:06 -0300

    fix

--- resources/views/admin/layout/head.blade.php
+++ resources/views/admin/layout/head.blade.php
@@ -8,10 +8,10 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="{{env('URL_SYSTEM')}}/css/admin.css">
+    <link rel="stylesheet" href="{{asset('css/admin.css')}}">
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+
 
     <!-- Google Font -->
     <link rel="stylesheet"
